51eea039489c5f6c05da39f7edbb522941aecb63
David <david.magania@gmail.com>
2022-01-18 13:50:51 -0600

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="entry-container pointer p-3"
+        @click="$router.push({name: 'entry', params: {id: 10}})"
+    >
+        <!-- titulo -->
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">18</span>
+            <span class="mx-1 fs-5">Enero</span>
+            <span class="mx-2 fw-light">2022, Martes</span>
+        </div>
+
+        <div class="entry-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis quidem recusandae laborum, sint eligendi assumenda, ullam eos cumque dicta nam delectus obcaecati. Sequi aut tempora provident neque voluptatum at deleniti!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover{
+        background-color: lighten($color: gray, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description{
+        font-size: 12px;
+    }
+}
+
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,32 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Sint consectetur voluptate irure dolore incididunt id sint elit sunt commodo enim adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Irure sunt aliquip duis nulla cupidatat. Enim deserunt cupidatat do aliqua id ut non labore aliqua.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Qui laboris id laborum in excepteur esse ad reprehenderit reprehenderit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 3000,
+            date: new Date().toDateString(),
+            text: 'Dolor sint minim velit proident eiusmod aliquip ullamco amet. Amet do est enim in amet elit amet.',
+            picture: null,
+        },
+    ]
+
+})
